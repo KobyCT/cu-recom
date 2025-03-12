8a95443a7aa1d11437fdb395df1a4632d41ea087
KobyCT <punnping54@gmail.com>
2025-03-13 00:59:04 +0700

    add

--- src/app/addItem/page.js
+++ src/app/addItem/page.js
@@ -4,7 +4,7 @@ import { useState } from 'react';
 import { FaHome, FaShoppingCart, FaPlus, FaComments, FaUser } from 'react-icons/fa';
 import NavItem from '@/app/component/Navbar';
 import HeaderSearchLess from '../component/headerns';
-
+import Link from 'next/link';
 const categories = [
   'เครื่องแต่งกาย',
   'อุปกรณ์เครื่องใช้ขนาดเล็ก',
@@ -25,15 +25,11 @@ export default function SellProductPage() {
       <main className="flex-1 flex flex-col items-center justify-center p-4">
         <h2 className="text-xl font-semibold mb-4 text-center">เลือกหมวดหมู่ของสินค้าที่ต้องการขาย</h2>
         <div className="w-full max-w-md">
-          {categories.map((category, index) => (
-            <button
-              key={index}
-              className="w-full bg-white p-3 mb-3 rounded-lg shadow-md text-gray-800 text-center hover:bg-gray-200 transition"
-              onClick={() => setSelectedCategory(category)}
-            >
-              {category}
-            </button>
-          ))}
+        <Link href="/addItem/wearable" className="block w-full bg-white p-3 mb-3 rounded-lg shadow-md text-gray-800 text-center hover:bg-gray-200 transition">เครื่องแต่งกาย</Link>
+        <Link href="/addItem/gadget" className="block w-full bg-white p-3 mb-3 rounded-lg shadow-md text-gray-800 text-center hover:bg-gray-200 transition">อุปกรณ์เครื่องใช้ขนาดเล็ก</Link>
+        <Link href="/addItem/big" className="w-full block bg-white p-3 mb-3 rounded-lg shadow-md text-gray-800 text-center hover:bg-gray-200 transition">อุปกรณ์เครื่องใช้ขนาดใหญ่</Link>
+        <Link href="/addItem/book" className="w-full block bg-white p-3 mb-3 rounded-lg shadow-md text-gray-800 text-center hover:bg-gray-200 transition">หนังสือและเอกสารการเรียน</Link>
+        <Link href="/addItem/draw" className="w-full block bg-white p-3 mb-3 rounded-lg shadow-md text-gray-800 text-center hover:bg-gray-200 transition">เครื่องเขียนและอุปกรณ์ศิลปะ</Link>
         </div>
       </main>
 
